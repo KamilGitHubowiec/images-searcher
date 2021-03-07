4538a809432c035c94363ce67aa6bb069f2df93b
KamilGitHubowiec <hantsky123@gmail.com>
2021-03-07 21:02:51 +0100

    Hid .env

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,7 @@
 import firebase from "firebase";
 
 const config = {
-  apiKey: "AIzaSyCURtE_e4eiLfvv94SWC-8YhhY3htP8Uqw",
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
   authDomain: "images-searcher-151c7.firebaseapp.com",
   databaseURL: "https://images-searcher-151c7-default-rtdb.firebaseio.com",
   projectId: "images-searcher-151c7",
